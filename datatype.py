bf6ad06eee56af0d843d8493b719ed5da3db69b1
Deve000 <rebuta.devesophia912@gmail.com>
2026-02-10 13:44:49 +0800

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,26 @@
+"""
+Activity #2
+"""
+
+name = 'Deve Sophia J. Rebuta'
+age = 18
+height = float(1.51)
+work = False
+
+print('\nStudent Personal Information\n\n')
+
+print("Full Name    :", name)
+print(type(name))
+print("\n")
+
+print("Age          :", age, "years old")
+print(type(age))
+print("\n")
+
+print("Height (in meters):", height, "m")
+print(type(height))
+print("\n")
+
+print("Currently working?", work)
+print(type(work))
+print("\n")
