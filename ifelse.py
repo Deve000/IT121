7c3fd4b8bd77e9d434e0abe2892d2487557a3652
Deve000 <rebuta.devesophia912@gmail.com>
2026-02-10 13:43:58 +0800

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,13 @@
+"""
+Deve Sophia J. Rebuta
+IT1R4
+
+Activity (Assignment) #1
+"""
+
+score = 100
+
+if score >= 75:
+    print('\nCongratulations, you have passed!\n')
+else:
+    print('\nYou have failed.\n')
