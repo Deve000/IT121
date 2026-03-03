0dd55d6eda4e42785a42238e1cfcf4d47f134e1e
Deve000 <rebuta.devesophia912@gmail.com>
2026-03-03 11:09:51 +0800

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,12 @@
+"""
+Deve Rebuta
+IT1R4
+
+User Input Activity
+"""
+
+name = (input('\nEnter name: '))
+age = int(input('Enter age: '))
+
+
+print('\nHello,', name, '! You are', age, 'years old.\n')
